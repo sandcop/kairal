1c52828d3fad50808a35e4b208db768bb153a45b
sandcop <145860117+sandcop@users.noreply.github.com>
2026-02-27 08:54:52 -0300

    Update PLACE_ID for Google Places API request

--- netlify/functions/reviews.js
+++ netlify/functions/reviews.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event) {
     const GOOGLE_API_KEY = process.env.GOOGLE_PLACES_KEY;
-    const PLACE_ID = 'ChIJuZY7ePPEYpYR9nPzAkc72BI';
+    const PLACE_ID = 'ChIJW9rJelQqrpYRMhzt6voouQE';
 
     try {
         const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${PLACE_ID}&fields=name,rating,reviews,user_ratings_total&language=es&key=${GOOGLE_API_KEY}`;

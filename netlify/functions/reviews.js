180c8d6e4b73cca779c1220d0d8c12e2bc075b13
sandcop <145860117+sandcop@users.noreply.github.com>
2026-02-27 08:20:40 -0300

    Add files via upload

---
+++ netlify/functions/reviews.js
@@ -0,0 +1,21 @@
+exports.handler = async function(event) {
+    const GOOGLE_API_KEY = process.env.GOOGLE_PLACES_KEY;
+    const PLACE_ID = 'ChIJuZY7ePPEYpYR9nPzAkc72BI';
+
+    try {
+        const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${PLACE_ID}&fields=name,rating,reviews,user_ratings_total&language=es&key=${GOOGLE_API_KEY}`;
+        const res = await fetch(url);
+        const data = await res.json();
+
+        return {
+            statusCode: 200,
+            headers: { 'Access-Control-Allow-Origin': 'https://kairal.cl' },
+            body: JSON.stringify(data)
+        };
+    } catch(err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: err.message })
+        };
+    }
+};

dcd14715d0af623f424fd976b9e40757168f5245
sandcop <145860117+sandcop@users.noreply.github.com>
2026-02-27 00:29:49 -0300

    Add files via upload

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -1,1 +1,41 @@
+exports.handler = async function(event) {
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
 
+    try {
+        const { email, firstName, lastName, recurso } = JSON.parse(event.body);
+
+        const response = await fetch('https://api.brevo.com/v3/contacts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'api-key': process.env.BREVO_API_KEY
+            },
+            body: JSON.stringify({
+                email,
+                attributes: {
+                    FIRSTNAME: firstName || '',
+                    LASTNAME: lastName || '',
+                    RECURSO_DESCARGADO: recurso || ''
+                },
+                listIds: [2],
+                updateEnabled: true
+            })
+        });
+
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            headers: { 'Access-Control-Allow-Origin': 'https://kairal.cl' },
+            body: JSON.stringify({ success: true, data })
+        };
+
+    } catch(err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ success: false, error: err.message })
+        };
+    }
+};
